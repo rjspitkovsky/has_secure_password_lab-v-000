5d87f54f555c974914a2e727637c5218e34b4a3f
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-21 20:46:04 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
 
   def new
-    @user = User.new 
+    @user = User.new
   end
 
   def create
-    @user = User.new(user_params).save 
+    @user = User.new(user_params).save
   end
 
   def welcome
   end
 
-  private 
+  private
 
-    def user_params 
-      params.require(:users).permit(:username, :password, :password_confirmation)
-    end 
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
 
 end
