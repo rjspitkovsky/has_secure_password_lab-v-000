43a3337c6a96c6a467663719ed8bd744010705a9
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-21 20:42:17 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,11 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+  get '/welcome' => 'users#welcome'
 end
