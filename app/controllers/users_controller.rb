ada5fb18cb972199b86302665ef073291b5eafaa
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-21 21:10:35 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
 
   def create
      @user = User.new(user_params).save
-     if !@user.nil?
       current_user = @user.id
       redirect_to '/welcome'
     else
