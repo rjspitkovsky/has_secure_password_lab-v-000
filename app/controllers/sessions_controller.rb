eed4aae3e2e5356045253c61c78b5428459a2c83
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-21 21:16:28 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
        @user = User.find_by(name: params[:name])
-      if @user.authenticate(params[:password])
+      return head(:forbidden) unless @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to '/welcome'
     else
