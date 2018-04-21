bf6e03f97c998fdd58eaf686fbff8ab01e8ca0bd
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-21 21:12:43 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
 
   def create
      @user = User.new(user_params)
-     if @user.save 
-      current_user = @user.id
+     if @user.save
+      session[:user_id] = @user.id
       redirect_to '/welcome'
     else
       redirect_to '/signup'
