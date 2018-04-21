b0e892b509c9a935b616b227317d9de2e639af33
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-21 20:52:39 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    session[:username] = @user.username 
   end
 
   def destroy
