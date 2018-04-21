e32a623b2df3f19e558838a98cf374b964132377
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-21 21:32:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= session[:user_id]
+    @current_user ||= User.find(session[:user_id])
   end
 end
