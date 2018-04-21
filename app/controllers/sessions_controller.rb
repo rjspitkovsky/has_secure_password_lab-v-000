a242571fba30c825f8546f9dc8faa27e1b702046
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-21 21:41:12 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,10 @@ class SessionsController < ApplicationController
         return head(:forbidden) unless @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
         redirect_to '/welcome'
-    else
-      redirect_to '/login'
+      else
+        redirect_to '/login'
+      end
     end
-  end
 
 
   def destroy
